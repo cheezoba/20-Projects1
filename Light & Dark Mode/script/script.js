ae9fc2c628aaa0c1b7369a1ad78e16efd507d778
Chizoba Odita <oditachizoba@gmail.com>
2024-03-13 11:58:31 +0100

    update

--- Light & Dark Mode/script/script.js
+++ Light & Dark Mode/script/script.js
@@ -37,12 +37,30 @@ function lightMode() {
 function switchTheme (event){
   if(event.target.checked){
     document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
     darkMode();
   }
   else{
     document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
     lightMode();
   }
 }
 // Event Listener
 toggleSwitch.addEventListener('change', switchTheme);
+
+// Check local storage for theme
+const currentTheme = localStorage.getItem('theme');
+
+if(currentTheme){
+  document.documentElement.setAttribute('data-theme', currentTheme);
+
+  if(currentTheme === 'dark'){
+    toggleSwitch.checked = true;
+    darkMode();
+  }
+  else{
+    toggleSwitch.checked = false;
+    lightMode();
+  }
+}
