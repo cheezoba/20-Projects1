d9a85de02b7f8a9a7f7e301b735431dadecef1a7
Chizoba Odita <oditachizoba@gmail.com>
2024-03-13 14:16:49 +0100

    New

--- Light & Dark Mode/script/script.js
+++ Light & Dark Mode/script/script.js
@@ -14,23 +14,20 @@ function imageMode(color){
   image3.src = `img/undraw_conceptual_idea_${color}.svg`;
 }
 
-// Dark Mode
-function darkMode() {
-  nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-  textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-  toggleIcon.children[0].textContent = 'Dark Mode';
-  toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
-  imageMode('dark');
+
+// Dark and Light Mode Styling
+function toggleDarkLightMode(isDark){
+  nav.style.backgroundColor =  isDark ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)';
+
+  textBox.style.backgroundColor = isDark ?'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)';
+
+  toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
+
+  isDark ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon') : toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+
+  isDark ? imageMode('dark') : imageMode('light');
 }
 
-// Light Mode
-function lightMode() {
-  nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-  textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-  toggleIcon.children[0].textContent = 'Light Mode';
-  toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
-  imageMode('light');
-}
 
 
 // Switch Theme
@@ -38,12 +35,12 @@ function switchTheme (event){
   if(event.target.checked){
     document.documentElement.setAttribute('data-theme', 'dark');
     localStorage.setItem('theme', 'dark');
-    darkMode();
+    toggleDarkLightMode(true);
   }
   else{
     document.documentElement.setAttribute('data-theme', 'light');
     localStorage.setItem('theme', 'light');
-    lightMode();
+    toggleDarkLightMode(false);
   }
 }
 // Event Listener
@@ -57,10 +54,10 @@ if(currentTheme){
 
   if(currentTheme === 'dark'){
     toggleSwitch.checked = true;
-    darkMode();
+    toggleDarkLightMode(true);
   }
   else{
     toggleSwitch.checked = false;
-    lightMode();
+    toggleDarkLightMode(false);
   }
-}
+} 
