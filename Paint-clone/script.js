8d9cda45f6198afa30560af76248ae4964b460ff
Chizoba Odita <oditachizoba@gmail.com>
2024-05-09 10:35:04 +0100

    new

--- Paint-clone/script.js
+++ Paint-clone/script.js
@@ -1,3 +1,4 @@
+const BRUSH_TIME = 1500;
 const activeToolEl = document.getElementById('active-tool');
 const brushColorBtn = document.getElementById('brush-color');
 const brushIcon = document.getElementById('brush');
@@ -89,7 +90,7 @@ clearCanvasBtn.addEventListener('click', () => {
   drawnArray = [];
   // Active Tool
   activeToolEl.textContent = 'Canvas Cleared';
-  setTimeout(switchToBrush, 1500);
+  setTimeout(switchToBrush, BRUSH_TIME);
 });
 
 // Draw what is stored in DrawnArray
@@ -170,7 +171,7 @@ saveStorageBtn.addEventListener('click', () => {
   localStorage.setItem('savedCanvas', JSON.stringify(drawnArray));
   // Active Tool
   activeToolEl.textContent = 'Canvas Saved';
-  setTimeout(switchToBrush, 1500);
+  setTimeout(switchToBrush, BRUSH_TIME);
 });
 
 // Load from Local Storage
@@ -180,10 +181,10 @@ loadStorageBtn.addEventListener('click', () => {
     restoreCanvas();
   // Active Tool
     activeToolEl.textContent = 'Canvas Loaded';
-    setTimeout(switchToBrush, 1500);
+    setTimeout(switchToBrush, BRUSH_TIME);
   }  else {
     activeToolEl.textContent = 'No Canvas Found';
-    setTimeout(switchToBrush, 1500);
+    setTimeout(switchToBrush, BRUSH_TIME);
   }
 });
 
@@ -192,7 +193,7 @@ clearStorageBtn.addEventListener('click', () => {
   localStorage.removeItem('savedCanvas');
   // Active Tool
   activeToolEl.textContent = 'Local Storage Cleared';
-  setTimeout(switchToBrush, 1500);
+  setTimeout(switchToBrush, BRUSH_TIME);
 });
 
 // Download Image
@@ -201,7 +202,7 @@ downloadBtn.addEventListener('click', () => {
   downloadBtn.download = 'Cheezoba-paint-works'
   // Active Tool
   activeToolEl.textContent = 'Image File Saved';
-  setTimeout(switchToBrush, 1500);
+  setTimeout(switchToBrush, BRUSH_TIME);
 });
 
 // Event Listener
